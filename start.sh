3fb7faa9198897f1ce034459fc5fe30c534de5fa
movaaafdk <158296329+movaaafdk@users.noreply.github.com>
2024-02-01 00:27:43 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -9,7 +9,7 @@ then
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /VJ-FILTER-BOT
 fi
-cd /VJ-FILTER-BOT
+cd /AUTO-FILTER-BOT
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
